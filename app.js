efdf185b4706ca09171bd0a8d7097e4d2ab9ebf0
CDocherty <cdocherty@ebay.com>
2018-04-16 14:13:04 +0100

    sort out bin

--- app.js
+++ app.js
@@ -1,50 +1,103 @@
-var io = require('socket.io'),
-    http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    app = express(),
-    server = http.createServer(app),
-    port = process.env.PORT || 3000;
+// var io = require('socket.io'),
+//     http = require('http'),
+//     path = require('path'),
+//     express = require('express'),
+//     app = express(),
+//     server = http.createServer(app),
+//     port = process.env.PORT || 3000;
 
-/**
- * Direct app to 
- * correct paths
- */
+// var index = require('./routes/index');
+
+// var app = express();
+
+// /**
+//  * Direct app to 
+//  * correct paths
+//  */
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+
+// var chat_room = io.listen(server);
+// var usernames = {};
+
+// server.listen(port);
+
+
+// /**
+//  * On connection start events
+//  * @param  {socket}
+//  * @return undefined
+//  */
+// chat_room.sockets.on('connection', function(socket) {
+
+//     socket.on('adduser', function(username, colour) {
+//         socket.username = username;
+//         socket.colour = colour;
+//         usernames[username] = username;
+
+//         socket.emit('updatechat', 'SERVER', 'Welcome to the chat!');
+//         socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+//         chat_room.sockets.emit('updateusers', usernames);
+//     });
+
+//     socket.on('disconnect', function() {
+//         delete usernames[socket.username];
+//         chat_room.sockets.emit('updateusers', usernames);
+//         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+//     });
+
+//     socket.on('sendchat', function(data) {
+//         chat_room.sockets.emit('updatechat', socket.username, data, socket.colour);
+//     });
+// });
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', index);
+app.use('/users', users);
 
-
-
-var chat_room = io.listen(server);
-var usernames = {};
-
-server.listen(port);
-
-
-/**
- * On connection start events
- * @param  {socket}
- * @return undefined
- */
-chat_room.sockets.on('connection', function(socket) {
-
-    socket.on('adduser', function(username, colour) {
-        socket.username = username;
-        socket.colour = colour;
-        usernames[username] = username;
-
-        socket.emit('updatechat', 'SERVER', 'Welcome to the chat!');
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-        chat_room.sockets.emit('updateusers', usernames);
-    });
-
-    socket.on('disconnect', function() {
-        delete usernames[socket.username];
-        chat_room.sockets.emit('updateusers', usernames);
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-    });
-
-    socket.on('sendchat', function(data) {
-        chat_room.sockets.emit('updatechat', socket.username, data, socket.colour);
-    });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,155 +1,165 @@
-/**
- * Set requires for this file
- */
-var sweetAlert = require('sweetalert');
-var $ = require('jquery');
-var moment = require('moment');
+// /**
+//  * Set requires for this file
+//  */
+// var sweetAlert = require('sweetalert');
+// var $ = require('jquery');
+// var moment = require('moment');
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    var socket = io.connect();
+//     var socket = io.connect();
 
-    var prompt = swal({
-        title: "Username please",
-        text: "What's your name?",
-        type: "input",
-        showCancelButton: false,
-        closeOnConfirm: true,
-        animation: "slide-from-top",
-        inputPlaceholder: "What's your name?"
-    });
-
-
-    /**
-     * Get random colour
-     * to assign to user
-     * @return colour
-     */
-    function getRandomColour() {
-        var letters = '0123456789ABCDEF'.split('');
-        var colour = '#';
-        for (var i = 0; i < 6; i++) {
-            colour += letters[Math.floor(Math.random() * 16)];
-        }
-        return colour;
-    }
+//     var prompt = swal({
+//         title: "Username please",
+//         text: "What's your name?",
+//         type: "input",
+//         showCancelButton: false,
+//         closeOnConfirm: true,
+//         animation: "slide-from-top",
+//         inputPlaceholder: "What's your name?"
+//     });
 
 
-    /**
-     * Set config for emojis
-     * including img dir
-     */
-    emojify.setConfig({
-        only_crawl_id: null, // Use to restrict where emojify.js applies
-        img_dir: 'https://github.global.ssl.fastly.net/images/icons/emoji/',
-        ignored_tags: { // Ignore the following tags
-            'SCRIPT': 1,
-            'TEXTAREA': 1,
-            'A': 1,
-            'PRE': 1,
-            'CODE': 1
-        }
-    });
+//     /**
+//      * Get random colour
+//      * to assign to user
+//      * @return colour
+//      */
+//     function getRandomColour() {
+//         var letters = '0123456789ABCDEF'.split('');
+//         var colour = '#';
+//         for (var i = 0; i < 6; i++) {
+//             colour += letters[Math.floor(Math.random() * 16)];
+//         }
+//         return colour;
+//     }
 
 
-    /**
-     * On socket connect launch
-     * alert box for username
-     * Emit add user to chat and assign
-     * colour
-     */
-    socket.on('connect', function() {
-        prompt;
-        // call the server-side function 'adduser' 
-        $('.confirm').click(function() {
-            var value = $('.sweet-alert input').val();
-            if (value === "") {
-                value = 'Anonymous';
-            }
-            socket.emit('adduser', value, getRandomColour());
-        });
-    });
+//     /**
+//      * Set config for emojis
+//      * including img dir
+//      */
+//     emojify.setConfig({
+//         only_crawl_id: null, // Use to restrict where emojify.js applies
+//         img_dir: 'https://github.global.ssl.fastly.net/images/icons/emoji/',
+//         ignored_tags: { // Ignore the following tags
+//             'SCRIPT': 1,
+//             'TEXTAREA': 1,
+//             'A': 1,
+//             'PRE': 1,
+//             'CODE': 1
+//         }
+//     });
 
 
-    /**
-     * @param  {username}
-     * @param  {data} - text value
-     * @param  {colour} - user colour
-     * @return append of html
-     */
-    socket.on('updatechat', function(username, data, colour) {
-        var date = new Date();
-        var prettyDate = moment(date).format('kk:mm');
-
-        $('#conversation').append('<li style="background-color:' + colour + '"><p><b>' + username + ':</b><p> ' + data + '</p><p class="date">'+ prettyDate +'</p></li>');
-        $("#conversation").scrollTop($("#conversation")[0].scrollHeight);
-        emojify.run();
-    });
-
-
-    /**
-     * Update users and add to html
-     * @param  {data}
-     * @return append user to html
-     */
-    socket.on('updateusers', function(data) {
-        $('#users').empty();
-        $.each(data, function(key, value) {
-            $('#users').append('<div>' + key + '</div>');
-        });
-    });
+//     /**
+//      * On socket connect launch
+//      * alert box for username
+//      * Emit add user to chat and assign
+//      * colour
+//      */
+//     socket.on('connect', function() {
+//         prompt;
+//         // call the server-side function 'adduser' 
+//         $('.confirm').click(function() {
+//             var value = $('.sweet-alert input').val();
+//             if (value === "") {
+//                 value = 'Anonymous';
+//             }
+//             socket.emit('adduser', value, getRandomColour());
+//         });
+//     });
 
 
-    /**
-     * On socket disconnect log
-     * message to chat
-     * @param  {data}
-     * @return log message
-     */
-    socket.on('exit', function(data) {
-        log_chat_message(data.message);
-    });
+//     /**
+//      * @param  {username}
+//      * @param  {data} - text value
+//      * @param  {colour} - user colour
+//      * @return append of html
+//      */
+//     socket.on('updatechat', function(username, data, colour) {
+//         var date = new Date();
+//         var prettyDate = moment(date).format('kk:mm');
+
+//         $('#conversation').append('<li style="background-color:' + colour + '"><p><b>' + username + ':</b><p> ' + data + '</p><p class="date">'+ prettyDate +'</p></li>');
+//         $("#conversation").scrollTop($("#conversation")[0].scrollHeight);
+//         emojify.run();
+//     });
 
 
-    /**
-     * If user div is empty
-     * hide from list
-     */
-    var nullDiv = $('#users div');
-
-    $.each(nullDiv, function(index, value) {
-        if (value.innerText === 'null') {
-            $(this).hide();
-        }
-    });
-
-
-    /**
-     * On button click send data
-     * and emit message
-     */
-    $('#datasend').click(function() {
-        var message = $('#data').val();
-        if (message.length) {
-            $('#data').val('');
-            // tell server to execute 'sendchat' and
-            // send along one parameter
-            socket.emit('sendchat', message);
-        }
-    });
+//     /**
+//      * Update users and add to html
+//      * @param  {data}
+//      * @return append user to html
+//      */
+//     socket.on('updateusers', function(data) {
+//         $('#users').empty();
+//         $.each(data, function(key, value) {
+//             $('#users').append('<div>' + key + '</div>');
+//         });
+//     });
 
 
-    /**
-     * On enter send data
-     * and emit message and 
-     * refocus
-     */
-    $('#data').keypress(function(e) {
-        if (e.which == 13) {
-            $(this).blur();
-            $('#datasend').focus().click();
-            $('#data').focus();
-        }
-    });
+//     /**
+//      * On socket disconnect log
+//      * message to chat
+//      * @param  {data}
+//      * @return log message
+//      */
+//     socket.on('exit', function(data) {
+//         log_chat_message(data.message);
+//     });
+
+
+//     /**
+//      * If user div is empty
+//      * hide from list
+//      */
+//     var nullDiv = $('#users div');
+
+//     $.each(nullDiv, function(index, value) {
+//         if (value.innerText === 'null') {
+//             $(this).hide();
+//         }
+//     });
+
+
+//     /**
+//      * On button click send data
+//      * and emit message
+//      */
+//     $('#datasend').click(function() {
+//         var message = $('#data').val();
+//         if (message.length) {
+//             $('#data').val('');
+//             // tell server to execute 'sendchat' and
+//             // send along one parameter
+//             socket.emit('sendchat', message);
+//         }
+//     });
+
+
+//     /**
+//      * On enter send data
+//      * and emit message and 
+//      * refocus
+//      */
+//     $('#data').keypress(function(e) {
+//         if (e.which == 13) {
+//             $(this).blur();
+//             $('#datasend').focus().click();
+//             $('#data').focus();
+//         }
+//     });
+// });
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+module.exports = router;
